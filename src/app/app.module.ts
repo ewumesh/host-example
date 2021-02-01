3adb35514340384057ad8ba00959f312d84639f0
umeshthapa12 <Dell@UMESH>
2021-02-01 18:10:01 +0545

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
